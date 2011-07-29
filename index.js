9b0e579d17b91a8fd618757ffb73f4f2a32cbcab
James Halliday <mail@substack.net>
2011-07-29 03:52:04 -0700

    sorting and a draw() step

--- index.js
+++ index.js
@@ -14,18 +14,45 @@ var exports = module.exports = function (canvas) {
 function Heat (canvas) {
     this.canvas = canvas;
     this.context = canvas.getContext("2d");
+    this.points = [];
+    this.radius = 20;
+    this.maximum = 0;
 }
 
-Heat.prototype.addPoint = function (x, y, value) {
-    var maximum = 100;
+Heat.prototype.addPoint = function (x, y, value, radius) {
+    this.points.push({ x : x, y : y, value : value, radius : radius });
+    if (value > this.maximum) this.maximum = value;
+    return this;
+};
+
+Heat.prototype.draw = function () {
+    var self = this;
+    
+    self.points
+        .sort(function (a, b) {
+            return a.value - b.value
+        })
+        .forEach(function (pt) {
+            self.drawPoint(pt);
+        })
+    ;
+    return self;
+};
+
+Heat.prototype.drawPoint = function (x, y, value, radius) {
+    if (typeof x === 'object') {
+        var pt = x;
+        x = pt.x, y = pt.y, value = pt.value, radius = pt.radius;
+    }
+    
     var ctx = this.context;
-    var radius = 50;
+    if (!radius) radius = this.radius;
     
     var g = ctx.createRadialGradient(x, y, 0, x, y, radius);
-    g.addColorStop(0, rgb(value / maximum, 1));
-    g.addColorStop(0.5, rgb(value / maximum - 1 / 12, 1));
-    g.addColorStop(0.8, rgb(value / maximum - 1 / 6, 0.2));
-    g.addColorStop(1, rgb(value / maximum - 1 / 3, 0));
+    g.addColorStop(0, rgb(value / this.maximum, 1));
+    g.addColorStop(0.5, rgb(value / this.maximum, 1));
+    g.addColorStop(0.85, rgb(value / this.maximum - 1 / 12, 0.2));
+    g.addColorStop(1, rgb(value / this.maximum - 1 / 6, 0));
     
     ctx.fillStyle = g;
     ctx.fillRect(x - radius, y - radius, radius * 2, radius * 2);
@@ -35,6 +62,8 @@ Heat.prototype.addPoint = function (x, y, value) {
 
 function rgb (v, a) {
     var theta = Math.min(270, Math.max(0, (1 - v) * 360));
+    if (v < 0.5) a *= Math.pow(2 * v, 2);
+    
     var rgba = convert.hsl2rgb(theta, 100, 50).concat(a);
     return 'rgba(' + rgba.join(',') + ')';
 }
--- main.js
+++ main.js
@@ -10,8 +10,13 @@ $(window).ready(function () {
     }).appendTo(document.body);
     
     var heat = heatmap(canvas.get(0));
-    heat
-        .addPoint(100, 100, 30)
-        .addPoint(400, 200, 80)
-    ;
+    
+    for (var i = 0; i < 100; i++) {
+        var x = Math.random() * 500;
+        var y = Math.random() * 500;
+        var value = Math.random();
+        heat.addPoint(x, y, value);
+    }
+    
+    heat.draw();
 });
